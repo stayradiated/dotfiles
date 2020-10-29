71994367b9cb5b8813a12bdbddfd82e4d80e1ce4
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-10-29 21:42:31 +1300

    checkin: update state

--- apps/exeditor/firefox/withexeditorhost.sh
+++ apps/exeditor/firefox/withexeditorhost.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-/usr/local/lib/node/bin/node /home/admin/.cache/npm/lib/node_modules/withexeditorhost/index.js
+/usr/local/lib/node/bin/node /usr/local/lib/node/lib/node_modules/withexeditorhost/index.js
