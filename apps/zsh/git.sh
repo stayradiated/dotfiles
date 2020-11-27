bd4505a47df693cb6ad0f9ad68fb59bcc5b82f74
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-11-27 20:33:56 +1300

    checkin: update state

--- apps/zsh/git.sh
+++ apps/zsh/git.sh
@@ -14,8 +14,19 @@ alias gfr='git pull --rebase'
 # force push
 alias gpF='git push --force-with-lease'
 
-# create a work-in-progress commit
-alias gcwip='git commit -m "wip @ $(date +''%D %T'')" --no-verify'
-
 # print local git branches
 alias gbl='git for-each-ref --color=always --sort=committerdate refs/heads/ --format=''%(HEAD) %(color:yellow)%(refname:short)|%(color:red)%(objectname:short)|%(color:reset)%(contents:subject)|%(color:blue)%(authorname)|%(color:green)%(committerdate:relative)%(color:reset)'' | column -t -s \|'
+
+# diff
+alias gd='git diff'
+alias gdc='git diff --cached'
+
+# status
+alias gs='git status'
+
+# add
+alias g.='git add .'
+
+# commit
+alias gc='git commit'
+alias gcwip='git commit -m "wip @ $(date +''%D %T'')" --no-verify'
