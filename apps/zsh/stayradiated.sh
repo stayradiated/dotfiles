278f4ff833634e9cb0f4f3eeee0231dfa0f158ac
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-06-11 15:13:54 +1200

    feat: use bin scripts instead of zsh functions

--- apps/zsh/stayradiated.sh
+++ apps/zsh/stayradiated.sh
@@ -1,4 +1,5 @@
 autoload -U promptinit; promptinit
+
 # pure prompt
 PURE_PROMPT_SYMBOL='»'
 PURE_PROMPT_VICMD_SYMBOL='«'
@@ -44,18 +45,14 @@ export _ZL_DATA="${HOME}/dotfiles/apps/zsh/state/zlua.enc"
 eval "$(lua5.3 /usr/local/bin/z.lua --init zsh)"
 alias j='z'
 
-# typos
-alias celar='clear'
-
 # watson
 export WATSON_DIR="${HOME}/dotfiles/apps/watson"
 
-#
-# Alias
-#
-
 # when exiting ranger, cd to the same dir
 alias ranger='source ranger'
+
+# typos
+alias celar='clear'
 
 # vim
 alias vi='nvim'
@@ -70,18 +67,7 @@ alias cat='bat'
 alias ping='prettyping --nolegend'
 
 # git
-alias gunshallow='git stash save && git fetch && git reset --hard origin/master'
-
 alias gup='git push -u origin HEAD'
-
-alias bluetooth-on='as-host bluetoothctl power on'
-alias bluetooth-off='as-host bluetoothctl power off'
-
-alias monitor-on='xrandr --output HDMI1 --mode 1920x1080 --left-of eDP1'
-alias monitor-off='xrandr --output eDP1 --mode 1920x1080 --output HDMI1 --off'
-
-alias laptop-monitor-on='xrandr --output eDP1 --mode 1920x1080'
-alias laptop-monitor-off='xrandr --output eDP1 --off'
 
 # journal shortcuts
 alias jj='cd $(journal-utils --root)'
@@ -93,104 +79,5 @@ alias jlmu='nvim $(journal-utils --list music)'
 alias jlwi='nvim $(journal-utils --list wishlist)'
 alias jlyt='nvim $(journal-utils --list youtube)'
 
-use-config-keyboard () {
-  setxkbmap us -variant colemak -option ctrl:nocaps -option altwin:swap_alt_win
-}
-
-set-alacritty-font () {
-  sd -f m "^(\s*family:).*$" "\$1 ${1}" ~/dotfiles/apps/alacritty/alacritty.yml
-}
-
-set-alacritty-colors () {
-  sd -f m "^(\s*colors:).*$" "\$1 *${1}" ~/dotfiles/apps/alacritty/alacritty.yml
-}
-
-use-monitor () {
-  use-ethernet
-  # bluetooth-on
-  use-config-keyboard
-  monitor-on
-  set-alacritty-font terminal
-  source ~/dotfiles/apps/bspwm/bspwmrc
-}
-
-use-laptop () {
-  use-wifi
-  # bluetooth-off
-  monitor-off
-  set-alacritty-font Gomme
-  source ~/dotfiles/apps/bspwm/bspwmrc
-}
-
-rsync-photos () {
-  DEVICE="${1:-/dev/sda1}"
-  as-host -t bash -c \
-    "sudo mkdir -p /mnt/usb; \
-    if mount | grep -q '${DEVICE}'; then \
-      echo 'Already mounted'; \
-    else \
-      sudo mount '${DEVICE}' /mnt/usb; \
-    fi; \
-    rsync -vua /mnt/usb/DCIM ~/src/camera; \
-    sudo umount /mnt/usb; \
-    echo 'sync complete, please remove the SD card :)'"
-}
-
-wifi-switch () {
-  as-host sudo netctl-auto switch-to $1
-}
-
-gcloud-stream-build () {
-  gcloud builds log --stream $(\
-    gcloud builds list --limit 1 --format 'value(id)' --ongoing
-  )
-}
-
-use-ethernet () {
-  as-host '
-    sudo ip link set enp0s31f6 up &&
-    sudo ip link set wlp2s0 down &&
-    sudo dhcpcd
-  '
-}
-
-use-wifi () {
-  as-host '
-    sudo ip link set wlp2s0 up &&
-    sudo ip link set enp0s31f6 down &&
-    sudo dhcpcd
-  '
-}
-
-date2stamp () {
-    date --utc --date "$1" +%s
-}
-
-stamp2date (){
-    date --utc --date "1970-01-01 $1 sec" "+%Y-%m-%d %T"
-}
-
-dateDiff (){
-    case $1 in
-        -s)   sec=1;      shift;;
-        -m)   sec=60;     shift;;
-        -h)   sec=3600;   shift;;
-        -d)   sec=86400;  shift;;
-        *)    sec=86400;;
-    esac
-    dte1=$(date2stamp $1)
-    dte2=$(date2stamp $2)
-    diffSec=$((dte2-dte1))
-    if ((diffSec < 0)); then abs=-1; else abs=1; fi
-    echo $((diffSec/sec*abs))
-}
-
-#
-# ==============================================================================
-# TMUX
-# ------------------------------------------------------------------------------
-# Defines tmux aliases
-# ==============================================================================
-#
-
+# tmux
 alias tm='tmux list-sessions && tmux attach -d || tmux new-session'
