f2e14ff74b3fe8725e4bdbe9b985ee9095a9dc78
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-08-02 11:19:13 +1200

    snapshot: 08/02/21 11:19:13

--- apps/zsh/stayradiated.sh
+++ apps/zsh/stayradiated.sh
@@ -10,6 +10,12 @@ export GOROOT=/usr/local/go
 
 export GPG_TTY=$(tty)
 
+if [ -n "$TMUX" ]; then
+    eval "$(tmux show-environment -s NVIM_LISTEN_ADDRESS)"
+else
+    export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
+fi
+
 #
 # Editors
 #
