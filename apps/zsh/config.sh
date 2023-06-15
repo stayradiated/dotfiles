9de7b7ca61d3d971e0cedbcad4ee44bb2b40e5f1
George Czabania <1140167+stayradiated@users.noreply.github.com>
2023-06-15 18:06:40 +0200

    snapshot: 06/15/23 18:06:40

--- apps/zsh/config.sh
+++ apps/zsh/config.sh
@@ -186,6 +186,9 @@ alias mkdir="${aliases[mkdir]:-mkdir} -p"
 # alias sa='alias | grep -i'
 alias type='type -a'
 
+alias rm='rip-rm'
+alias realrm='/usr/bin/rm'
+
 alias mv="${aliases[mv]:-mv} -i"
 alias cp="${aliases[cp]:-cp} -i"
 alias ln="${aliases[ln]:-ln} -i"
