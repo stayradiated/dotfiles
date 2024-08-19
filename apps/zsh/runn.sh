f5b4736c623b212f0aa295df0077bd7033375f69
George Czabania <1140167+stayradiated@users.noreply.github.com>
2024-08-19 14:05:10 +1200

    snapshot: 08/19/24 14:05:10

---
+++ apps/zsh/runn.sh
@@ -0,0 +1,10 @@
+#
+# Runn Fast
+#
+
+# runn-cli
+export RUNN_DIR=/home/admin/src/github.com/Runn-Fast/runn
+alias r='runn'
+
+# local api keys
+export RUNN_LOCAL_API_KEY=API_local_6
--- apps/zsh/stayradiated.sh
+++ apps/zsh/stayradiated.sh
@@ -25,10 +25,6 @@ export GPG_TTY=$(tty)
 export NODE_CLIENT_LOG_FILE=/tmp/coc-nod-client.log
 export NVIM_COC_LOG_FILE=/tmp/coc-nvim.log
 
-# runn-cli
-export RUNN_DIR=/home/admin/src/github.com/Runn-Fast/runn
-alias r='runn'
-
 # fzf
 export FZF_DEFAULT_COMMAND='fd --type f'
 
